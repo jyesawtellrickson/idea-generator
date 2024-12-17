d64434d1c9d6c3d31f275f77459ad9c43e558269
Jye <jyesawtellrickson@gmail.com>
2024-12-17 21:39:57 +0800

    Test APIs with different counts

--- tests/test_apis.py
+++ tests/test_apis.py
@@ -5,27 +5,29 @@ from src.utils.api_helpers import (
     get_pubmed_papers,
 )
 
+num_results_test = 2
+
 
 def test_arxiv():
-    results = get_arxiv_papers("machine learning", 5)
-    assert len(results) == 5
+    results = get_arxiv_papers("machine learning", num_results_test)
+    assert len(results) == num_results_test
     assert all([key in results[0].keys() for key in ["title", "summary"]])
 
 
 def test_springer_nature():
-    results = get_springer_papers("machine learning", 5)
-    assert len(results) == 5
+    results = get_springer_papers("machine learning", num_results_test)
+    assert len(results) == num_results_test
     assert all([key in results[0].keys() for key in ["title", "abstract"]])
 
 
 def test_ieee():
-    results = get_ieee_papers("machine learning", 5)
-    assert len(results) == 5
+    results = get_ieee_papers("machine learning", num_results_test)
+    assert len(results) == num_results_test
     assert all([key in results[0].keys() for key in ["title", "summary"]])
 
 
 def test_pubmed():
-    results = get_pubmed_papers("machine learning", 5)
+    results = get_pubmed_papers("machine learning", num_results_test)
     print(results)
-    assert len(results) == 5
+    assert len(results) == num_results_test
     assert all([key in results[0].keys() for key in ["title", "sortpubdate"]])

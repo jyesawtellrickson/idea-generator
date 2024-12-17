09fed448e5d6183d7234fb457ae444da71868ab4
Jye <jyesawtellrickson@gmail.com>
2024-12-17 21:54:36 +0800

    Test generator agent

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -4,6 +4,10 @@ from langgraph.prebuilt import create_react_agent
 
 from langgraph.checkpoint.memory import MemorySaver
 
+from src.agents.generator import gen_idea_generator_agent
+
+import argparse
+
 
 def check_weather(location: str, at_time: datetime | None = None) -> str:
     """Return the weather forecast for the specified location."""
@@ -75,3 +79,15 @@ def test_memory_agent():
     message = print_stream(graph, inputs2, config)
     assert "sunny" in message.content.lower()
     assert "sf" in message.content.lower()
+
+
+def test_generator_agent():
+    parser = argparse.ArgumentParser(description="Generate Research Ideas")
+    parser.add_argument("--model", type=str, default="mistral", choices=["mistral", "qwen2.5:14b"])
+    args = parser.parse_args()
+
+    tools = []
+
+    agent = gen_idea_generator_agent(args, tools)
+    response = agent.invoke("hello")
+    print(response)
